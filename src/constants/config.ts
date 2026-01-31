6f385c9e7c3ec61e959e2c9eaf35e6d4ab43095d
Juan Basgall <juanangelbasgall@hotmail.com>
2026-01-31 02:00:17 -0300

    initial commit

---
+++ src/constants/config.ts
@@ -0,0 +1,14 @@
+import * as pj from '../../package.json'
+
+export const config = {
+    project: {
+        alias: "Mu Campana",
+        icon: "",
+        name: pj.name,
+        version: pj.version
+    },
+    server: {
+        version: "99b + DL"
+    },
+    apiUrl: "http://93.127.142.139"
+}
